fab7e0aa2f9a2542ef4f80823139e3228674b2f3
Ourai Lin <ourairyu@gmail.com>
2021-12-05 20:35:39 +0800

    feat: `DataTable` component

---
+++ src/data-table/DataTable.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from 'react';
+import { DataTableStructuralComponent } from '@zora/core/dist/data-table';
+import { Table,TableColumnType } from 'antd';
+
+export default class DataTable extends DataTableStructuralComponent {
+  private resolveColumns(): TableColumnType<Record<string, any>>[] {
+    return (this.props.columns || []).map(col => ({}));
+  }
+
+  public render(): ReactNode {
+    const { className } = this.props;
+
+    return (
+      <div className={ ['DataTable', className].join(' ') }>
+        <div className="DataTable-tableWrapper">
+          <Table
+            className="DataTable-table"
+            columns={ this.resolveColumns() }
+          />
+        </div>
+      </div>
+    );
+  }
+}
